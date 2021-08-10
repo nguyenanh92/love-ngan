d6f8d080c785efb4c5d02561296a501f413f0015
nguyenanh92 <work.nguyenanh92@gmail.com>
2021-08-11 02:18:20 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+music = ['ido', 'noinaycoanh'];
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var days = document.querySelector('.days span');
+  var hour = document.querySelector('.hour');
+  var min = document.querySelector('.min');
+  var second = document.querySelector('.second');
+
+  var startDate = new Date(2021, 05, 12);
+  days.innerText = Math.floor((new Date - startDate)/86400000);
+  countTime();
+
+  function countTime() {
+    let today = new Date();
+    let ms = (today - startDate) % 86400000;
+    hour.innerText = Math.floor(ms / 3600000);
+    min.innerText = Math.floor(ms % 3600000 / 60000);
+    second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+  }
+
+  setInterval(countTime, 1000);
+  document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+
+  
+}, false);
+
+var HeartsBackground = {
+  heartHeight: 60,
+  heartWidth: 64,
+  hearts: [],
+  heartImage: './img/ntnw5.png',
+  maxHearts: 52,
+  minScale: 0.4,
+  draw: function() {
+    this.setCanvasSize();
+    this.ctx.clearRect(0, 0, this.w, this.h);
+    for (var i = 0; i < this.hearts.length; i++) {
+      var heart = this.hearts[i];
+      heart.image = new Image();
+      heart.image.style.height = heart.height;
+      heart.image.src = this.heartImage;
+      this.ctx.globalAlpha = heart.opacity;
+      this.ctx.drawImage (heart.image, heart.x, heart.y, heart.width, heart.height);
+    }
+    this.move();
+  },
+  move: function() {
+    for(var b = 0; b < this.hearts.length; b++) {
+      var heart = this.hearts[b];
+      heart.y += heart.ys;
+      if(heart.y > this.h) {
+        heart.x = Math.random() * this.w;
+        heart.y = -1 * this.heartHeight;
+      }
+    }
+  },
+  setCanvasSize: function() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.w = this.canvas.width;
+    this.h = this.canvas.height;
+  },
+  initialize: function() {
+    this.canvas = $('#canvas')[0];
+
+    if(!this.canvas.getContext)
+      return;
+
+    this.setCanvasSize();
+    this.ctx = this.canvas.getContext('2d');
+
+    for(var a = 0; a < this.maxHearts; a++) {
+      var scale = (Math.random() * (1 - this.minScale)) + this.minScale;
+      this.hearts.push({
+        x: Math.random() * this.w,
+        y: Math.random() * this.h,
+        ys: Math.random() + 1,
+        height: scale * this.heartHeight,
+        width: scale * this.heartWidth,
+        opacity: scale
+      });
+    }
+
+    setInterval($.proxy(this.draw, this), 30);
+  }
+};
+
+$(document).ready(function(){
+  HeartsBackground.initialize();
+});
